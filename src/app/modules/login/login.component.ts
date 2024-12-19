f77300c60ebe109f39079fea854173993fe58f45
B_Kamoladdin <creeddevx@gmail.com>
2024-12-19 17:54:28 +0500

    kamoladding yuvardi0

--- src/app/modules/login/login.component.ts
+++ src/app/modules/login/login.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
 export class LoginComponent {
+  nameLogin: string = '';
+  passLogin: string = '';
 
+  onSubmit() {
+    this.nameLogin;
+    this.passLogin;
+  }
+  onClick() {
+    console.log([this.nameLogin, this.passLogin]);
+  }
 }
