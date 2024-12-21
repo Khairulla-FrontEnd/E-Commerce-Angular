aafda73fcceb832fd8694c079771ffb96079380f
B_Kamoladdin <creeddevx@gmail.com>
2024-12-21 17:41:32 +0500

    swiper

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,12 +1,14 @@
 import { Component } from "@angular/core";
 import { HeroComponent } from "./components/hero/hero.component";
+import { FlashSectionComponent } from "./components/hero/main/flash-section/flash-section.component";
 
 @Component({
     selector:'app-home',
     standalone:true,
-    imports:[
-        HeroComponent
-    ],
+    imports: [
+    HeroComponent,
+    FlashSectionComponent
+],
     templateUrl:'./home.component.html',
     styleUrl:'./home.component.scss'
 })
