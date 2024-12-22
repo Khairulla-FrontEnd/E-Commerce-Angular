93a759c20512601aaff71351d7af7e37daae1f20
Khairulla-FrontEnd <khairulla2008@outlook.com>
2024-12-22 18:18:31 +0500

    added wishlist page

--- src/@core/layout/layout.routes.ts
+++ src/@core/layout/layout.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from "@angular/router";
 import { SignupComponent } from "../../app/modules/singup/signup.component";
 import { LoginComponent } from "../../app/modules/login/login.component";
 import { homeRoutes } from "../../app/modules/home/home.routes";
+import { WishlistComponent } from "../../app/modules/wishlist/wishlist.component";
 
 export const layoutRoutes:Routes = [
     {
@@ -17,5 +18,9 @@ export const layoutRoutes:Routes = [
     path:'login',
     component:LoginComponent
     },
-    ...homeRoutes
+    ...homeRoutes,
+    {
+        path:'wishlist',
+        component:WishlistComponent,
+    }
 ];
