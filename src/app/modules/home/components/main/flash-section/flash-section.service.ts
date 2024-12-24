615ebe0d6bf0e5a816ff2ed121bcd8cfe6be59f9
Khairulla-FrontEnd <khairulla2008@outlook.com>
2024-12-24 21:58:21 +0500

    added something I didnt notice :(

--- src/app/modules/home/components/main/flash-section/flash-section.service.ts
+++ src/app/modules/home/components/main/flash-section/flash-section.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from "@angular/core";
 import { ApiService } from "../../../../../shared/service/api.service";
-import { Observable, map } from "rxjs";
+import { Observable } from "rxjs";
 
 
 @Injectable({ providedIn:'root' })
@@ -9,36 +9,7 @@ export class FlashSectionService {
     constructor(private apiService:ApiService) { }
 
     getProducts(params:any = {}):Observable<any> {
-        return this.apiService.get('/products',params)
-        .pipe(
-            map((val:any) => {
-            const newVal = val.map((item:any,index:number) => {
-                  const image = item.images[0];
-                    const newImg = image.split("")
-                    .filter
-                (
-                    (item:any,index:number) => 
-                    item !== "\"" 
-                    && item !== "[" 
-                    && item !== "]"
-                )
-                    .join("");
-                    item.images = newImg;
-                    item.icon = "bi-heart";
-                    if
-                    (
-                        newImg === "https://placeimg.com/640/480/any" 
-                        || 
-                        newImg === "www.apple.com"
-                    ){
-                      return;
-                    }else{
-                      return item;
-                    }
-                })
-            return newVal.filter((item:any,index:number) => item !== undefined).splice(0,12);
-            })
-          );
+        return this.apiService.get('/products',params);
     }
     getProductById(productId:number): Observable<any> {
         return this.apiService.get('/products/' + productId);
