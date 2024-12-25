bcd9cd2f106c4088ea0a560e83eea34582e17908
Khairulla-FrontEnd <khairulla2008@outlook.com>
2024-12-25 23:00:13 +0500

    added hero swipers

--- src/app/modules/home/home.routing.ts
+++ src/app/modules/home/home.routing.ts
@@ -1,7 +1,6 @@
 import { AuthGuard } from "../auth/auth.guard";
 import { updateRoutes } from "../../shared/utils/route.utils";
 import { Resources } from "../../resources";
-import { Routes } from '@angular/router';
 
 export default updateRoutes([
     {
