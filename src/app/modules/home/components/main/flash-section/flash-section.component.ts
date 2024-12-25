aee5b405bd1999d84f63c58cb5c5469c741ededd
Khairulla-FrontEnd <khairulla2008@outlook.com>
2024-12-25 20:09:22 +0500

    optimized code

--- src/app/modules/home/components/main/flash-section/flash-section.component.ts
+++ src/app/modules/home/components/main/flash-section/flash-section.component.ts
@@ -1,85 +1,17 @@
-import { Component, inject } from '@angular/core';
-import { Navigation, Pagination } from 'swiper/modules';
-import Swiper from 'swiper';
-import { Observable } from 'rxjs';
-import { ProductCardComponent } from '../../../../../shared/components/product-card/product-card.component';
+import { Component } from '@angular/core';
 import { HeadingComponent } from '../../../../../shared/components/heading/heading.component';
-import { BaseLoadComponent } from '../../../../../shared/components/classes/base-load.component';
-import { FlashSectionService } from './flash-section.service';
-import { Skeleton } from 'primeng/skeleton';
 import { FlashSwiperComponent } from "./components/flash-swiper/flash-swiper.component";
 
 @Component({
   selector: 'app-flash-section',
   standalone: true,
-  imports: [ProductCardComponent, HeadingComponent, Skeleton, FlashSwiperComponent],
+  imports: [HeadingComponent, FlashSwiperComponent],
   templateUrl: './flash-section.component.html',
   styleUrl: './flash-section.component.scss',
 })
-export class FlashSectionComponent extends BaseLoadComponent<any> {
-  service = inject(FlashSectionService);
-  isActive: number = -1;
-
-  override afterLoadData(data: any): void {
-    var swiper = new Swiper('.mySwiper', {
-      cssMode: true,
-      slidesPerView: 1,
-      spaceBetween: 10,
-      freeMode: true,
-      navigation: {
-        nextEl: '.right',
-        prevEl: '.left',
-      },
-      modules: [Navigation, Pagination],
-    });
-    
-    
-    const newVal = data.map((item: any, index: number) => {
-      const image = item.images[0];
-      const newImg = image
-        .split('')
-        .filter(
-          (item: any, index: number) =>
-            item !== '"' && item !== '[' && item !== ']'
-        )
-        .join('');
-      item.images = newImg;
-      item.icon = 'bi-heart';
-      if (
-        newImg === 'https://placeimg.com/640/480/any' ||
-        newImg === 'www.apple.com'
-      ) {
-        return;
-      } else {
-        return item;
-      }
-    });
-    const newData = newVal.filter(
-      (item: any, index: number) => item !== undefined
-    );
-    this.data.set(newData);
-  }
-
-  getData(): Observable<any> {
-    return this.service.getProducts();
-  }
-
+export class FlashSectionComponent {
   allBtn: boolean = false;
   AllClickbtn() {
     this.allBtn = !this.allBtn;
-  }
-  handleLoad() {
-    var swiper2 = new Swiper('.mySwiper3', {
-      cssMode: true,
-      slidesPerView: 1,
-      spaceBetween: 10,
-      freeMode: true,
-      navigation: {
-        nextEl: '.right3',
-        prevEl: '.left3',
-      },
-      modules: [Navigation, Pagination],
-    });
-    
   }
 }
