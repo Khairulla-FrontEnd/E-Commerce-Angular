73698fe495e6e692b1393cadc8d279ece9ad11af
Khairulla-FrontEnd <khairulla2008@outlook.com>
2025-01-03 09:16:02 +0500

    fixed mobile issues

--- src/@core/layout/components/mobile/mobile.component.ts
+++ src/@core/layout/components/mobile/mobile.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { RouterLink } from '@angular/router';
 
 @Component({
@@ -11,10 +11,14 @@ import { RouterLink } from '@angular/router';
     styleUrl:'./mobile.component.scss'
 })
 
-export class MobileComponent{
+export class MobileComponent implements OnInit{
     icons:string[] = ['house','search','gear','heart','cart'];
     link:string[] = ['/home','/search','/settings','/wishlist','/cart'];
-    activePath:string = window.location.pathname;
+    activePath:string = '';
+
+    ngOnInit(): void {
+        this.activePath = window.location.pathname;
+    }
 
     setActive(link:string): void {
         this.activePath = link;
